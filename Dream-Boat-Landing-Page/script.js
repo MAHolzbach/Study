4303c26efc16eb49a9ca8401c838609b292968e9
Mike <maholzbach@gmail.com>
2017-03-29 18:45:59 +0200

    Started on booking form

--- Dream-Boat-Landing-Page/script.js
+++ Dream-Boat-Landing-Page/script.js
@@ -1,11 +1,5 @@
-$("#riverInfoButton").click(function() {
-  $(".thumbnail").hide("drop", {direction: "down"}, 500, function() {
-    $("#riverInfo").show("fade", 500);
-  });
-});
-
-$("#riverInfo").click(function() {
-  $(this).hide("fade", 500, function() {
-    $(".thumbnail").show("fade", 500);
-  });
-});
+// $("#bookingButton").click(function() {
+//   $("#lake-info").hide("drop", {direction: "down"}, 500, function() {
+//     $("#booking-info").show("fade", 500);
+//   });
+// });
