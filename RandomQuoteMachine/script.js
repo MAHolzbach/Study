0cadd02a43c1c908aee58635ca879451a7dd072c
Mike <maholzbach@gmail.com>
2017-04-14 15:10:22 +0200

    Fixed it

--- RandomQuoteMachine/script.js
+++ RandomQuoteMachine/script.js
@@ -1,20 +1,9 @@
 $(document).ready(function() {
-    $('#getMessage').on('click', function(){
-      console.log('clicked');
-      $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(json) {
-        console.log(json);
-        $('.talk').html(JSON.stringify(json));
-        });
-        // var html = '';
-        // Object.keys(json).forEach(function(val){
-        //   var data = json[val];
-        //   html += "<div class = 'quote'>";
-        //   data.forEach(function(item){
-        //     html += "<p>" + val.item + val.item + "<p>";
-        //   });
-        //   html += "</div>";
-        // });
-        // $('.talk').html(html);
-
-      });
+  $('#getMessage').on('click', function(){
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(json) {
+      var quotation = json.quoteText;
+      var author = json.quoteAuthor;
+      $('.quote').html('"' + quotation + '"' + '</br>' + '-' + author);
     });
+  });
+});
