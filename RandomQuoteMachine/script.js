1a00113400ff98c55fbc37b80a77cab4861265a5
Mike <maholzbach@gmail.com>
2017-04-15 19:37:09 +0200

    Practice
    Study all day on a holiday? The struggle is real...

--- RandomQuoteMachine/script.js
+++ RandomQuoteMachine/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+  var quotation;
+  var author;
   $('#getMessage').on('click', function(){
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(json) {
-      var quotation = json.quoteText;
-      var author = json.quoteAuthor;
+      quotation = json.quoteText;
+      author = json.quoteAuthor;
       $('.quote').html('"' + quotation + '"' + '</br>' + '-' + author);
     });
+  });
+
+  $('.shareQuote').on('click', function(event){
+    event.preventDefault();
+    if(quotation || author) {
+      window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quotation + '-' +  author));
+    } else {
+      return;
+    }
   });
 });
