4dd69f88392c364f48d2c5ada5bf50735069e3aa
Mike Holzbach <maholzbach@gmail.com>
2018-01-26 11:54:12 -0500

    Algos solved

--- AlgoProblems/exercises/maxchar/index.js
+++ AlgoProblems/exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+///////////////////SOLUTIONS/////////////////
+
+function maxChar(str) {
+  const chars = {};
+
+  for (let char of str) {
+    !chars[char] ? (chars[char] = 1) : chars[char]++;
+  }
+
+  let answer = 0;
+  let maxChar = "";
+
+  for (let prop in chars) {
+    chars[prop] > answer ? (answer = chars[prop]) && (maxChar = prop) : answer;
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
