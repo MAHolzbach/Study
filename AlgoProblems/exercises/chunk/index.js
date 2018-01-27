02b8bae57f616416a5e65d54ab4e42c814d58696
Mike Holzbach <maholzbach@gmail.com>
2018-01-27 17:19:42 -0500

    Algo solved

--- AlgoProblems/exercises/chunk/index.js
+++ AlgoProblems/exercises/chunk/index.js
@@ -8,6 +8,47 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+////////////SOLUTIONS//////////////
+
+// const chunk = (array, size) => {
+//   let finalArray = [];
+//   let workingArray = [];
+
+//   array.forEach(item => {
+//     workingArray.length < size
+//       ? workingArray.push(item)
+//       : finalArray.push(workingArray) &&
+//         (workingArray = []) &&
+//         workingArray.push(item);
+//   });
+//   finalArray.push(workingArray);
+//   return finalArray;
+// };
+
+// const chunk = (array, size) => {
+//   let chunked = [];
+
+//   for (let el of array) {
+//     let last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([el]);
+//     } else {
+//       last.push(el);
+//     }
+//   }
+//   return chunked;
+// };
+
+const chunk = (array, size) => {
+  let chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+};
 
 module.exports = chunk;
