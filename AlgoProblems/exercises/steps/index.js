74370b2dac93bdbd9d5586faf179e98423299c8e
Mike Holzbach <maholzbach@gmail.com>
2018-02-06 14:11:44 -0500

    Add book solution using recursion

--- AlgoProblems/exercises/steps/index.js
+++ AlgoProblems/exercises/steps/index.js
@@ -34,17 +34,34 @@
 //   }
 // };
 
-const steps = n => {
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
-    console.log(stair);
+// const steps = n => {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// };
+
+//USING RECURSION:
+const steps = (n, row = 0, stair = "") => {
+  if (row === n) {
+    return;
   }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
 };
 module.exports = steps;
