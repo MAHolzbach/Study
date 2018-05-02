7e4273a66a53dc585dbb20f138cc05a5f2f9fe15
Mike Holzbach <maholzbach@gmail.com>
2018-05-02 07:12:15 -0400

    Added UI class

--- traversy-course/github-finder/app.js
+++ traversy-course/github-finder/app.js
@@ -1,9 +1,21 @@
+const github = new GitHub();
+const ui = new UI();
+
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup', e => {
   let userText = e.target.value;
 
   if (userText !== '') {
-    console.log(userText);
+    github.getUser(userText).then(data => {
+      if (data.profile.message === 'Not Found') {
+        //Show error alert
+      } else {
+        //Show profile
+        ui.showProfile(data.profile);
+      }
+    });
+  } else {
+    //Clear profile
   }
 });
