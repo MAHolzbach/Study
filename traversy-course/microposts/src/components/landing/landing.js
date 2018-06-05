69af8a0082ee471601fc16b21445af4f3db3f7b5
Mike Holzbach <maholzbach@gmail.com>
2018-06-05 06:51:41 -0400

    Added initial UI class for dummy posts

--- traversy-course/microposts/src/components/landing/landing.js
+++ traversy-course/microposts/src/components/landing/landing.js
@@ -1,10 +1,38 @@
 import { http } from '../../http.js';
 
+class UI {
+  constructor() {
+    this.post = document.querySelector('#posts');
+    this.titleInput = document.querySelector('#title');
+    this.bodyInput = document.querySelector('#body');
+    this.idInput = document.querySelector('#id');
+    this.postInput = document.querySelector('.post-submit');
+    this.forState = 'add';
+  }
+  showPosts = posts => {
+    let output = '';
+
+    posts.forEach(post => {
+      output += `
+      <div class="card mb-3">
+        <div class="card-body">
+          <h4 class="card-title">${post.title}</h4>
+          <p class="card-text">${post.body}</p>
+        </div>
+      </div>
+      `;
+    });
+    this.post.innerHTML = output;
+  };
+}
+
+const ui = new UI();
+
 const getPosts = () => {
   http
     .get('http://localhost:3000/posts')
-    .then(data => console.log(data))
-    .cathc(err => {
+    .then(data => ui.showPosts(data))
+    .catch(err => {
       console.log(err);
     });
 };
