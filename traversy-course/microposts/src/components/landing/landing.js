57e9ffbf20df1a933b24129e89034d45a48b4c17
Mike Holzbach <maholzbach@gmail.com>
2018-06-06 07:17:43 -0400

    Add post, clear fields functionality

--- traversy-course/microposts/src/components/landing/landing.js
+++ traversy-course/microposts/src/components/landing/landing.js
@@ -18,11 +18,28 @@ class UI {
         <div class="card-body">
           <h4 class="card-title">${post.title}</h4>
           <p class="card-text">${post.body}</p>
+          <a href='#' class="edit card-link" data-id="${
+            post.id
+          }"><i class="fa fa-edit"></i></a>
+          <a href='#' class="delete card-link" data-id="${
+            post.id
+          }"><i class="fa fa-trash-alt"></i></a>
         </div>
       </div>
       `;
     });
     this.post.innerHTML = output;
+  };
+  showAlert = (msg, styleClasses) => {
+    let alertCard = '';
+  };
+  clearAlert = () => {};
+  clearFields = () => {
+    const title = document.querySelector('#title');
+    const body = document.querySelector('#body');
+
+    title.value = '';
+    body.value = '';
   };
 }
 
@@ -37,4 +54,22 @@ const getPosts = () => {
     });
 };
 
+const submitPost = () => {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  const post = {
+    title,
+    body
+  };
+  http
+    .post('http://localhost:3000/posts', post)
+    .then(() => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+};
+
 document.addEventListener('DOMContentLoaded', getPosts);
+document.querySelector('.post-submit').addEventListener('click', submitPost);
