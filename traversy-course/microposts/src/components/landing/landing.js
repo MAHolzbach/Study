8577b8eaff5544be6c6d0754e7b15d2efefb2835
Mike Holzbach <maholzbach@gmail.com>
2018-06-09 14:22:07 -0400

    Added editing logic

--- traversy-course/microposts/src/components/landing/landing.js
+++ traversy-course/microposts/src/components/landing/landing.js
@@ -6,7 +6,7 @@ class UI {
     this.titleInput = document.querySelector('#title');
     this.bodyInput = document.querySelector('#body');
     this.idInput = document.querySelector('#id');
-    this.postInput = document.querySelector('.post-submit');
+    this.postSubmit = document.querySelector('.post-submit');
     this.forState = 'add';
   }
   showPosts = posts => {
@@ -59,6 +59,30 @@ class UI {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id;
+    this.changeFormState('edit');
+  };
+  clearIdInput = () => {
+    this.idInput.value = '';
+  };
+  changeFormState = type => {
+    const button = document.createElement('button');
+    const cardForm = document.querySelector('.card-form');
+    const formEnd = document.querySelector('.form-end');
+    if (type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-block btn-warning';
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post it!';
+      this.postSubmit.className = 'post-submit btn btn-block btn-primary';
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      this.clearIdInput();
+      this.clearFields();
+    }
   };
 }
 
@@ -76,18 +100,32 @@ const getPosts = () => {
 const submitPost = () => {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
   const post = {
     title,
     body
   };
-  http
-    .post('http://localhost:3000/posts', post)
-    .then(() => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  if (post.title === '' || post.body === '') {
+    ui.showAlert('All fields must be filled!', 'alert alert-danger');
+  } else {
+    id === ''
+      ? http
+          .post('http://localhost:3000/posts', post)
+          .then(() => {
+            ui.showAlert('Post added', 'alert alert-success');
+            ui.clearFields();
+            getPosts();
+          })
+          .catch(err => console.log(err))
+      : http
+          .put(`http://localhost:3000/posts/${id}`, post)
+          .then(() => {
+            ui.showAlert('Post updated', 'alert alert-success');
+            ui.changeFormState('add');
+            getPosts();
+          })
+          .catch(err => console.log(err));
+  }
 };
 
 const enableEdit = e => {
@@ -105,6 +143,14 @@ const enableEdit = e => {
   }
 };
 
+const cancelEdit = e => {
+  e.preventDefault();
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+};
+
 document.addEventListener('DOMContentLoaded', getPosts);
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 document.querySelector('#posts').addEventListener('click', enableEdit);
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
